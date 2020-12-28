d1100f49b86a889479c0544b3eeb5e5409b72304
andersson601sp <59657083+andersson601sp@users.noreply.github.com>
2020-12-28 11:42:58 -0300

    Add files via upload

---
+++ PROJETO BACKEND/Email.DML/Contato.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Email.DML
+{
+    public class Contato
+    { 
+
+        protected byte _id;
+        public byte Id{ get { return _id; } set { _id = value; }}
+
+        private string _nome;
+        public string Nome { get { return _nome; } set { _nome = value; } }
+
+        private string _email;
+        public string Email { get { return _email; } set { _email = value; } }
+
+    }
+}
